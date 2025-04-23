9d5d7cc89bf704d9bbe1a279824da38f16d1a22f
esteban027 <bermu095@outlook.com>
2025-04-22 19:42:03 -0500

    Proyect Initialization

---
+++ SpaceFlightChallenge/SpaceFlightChallenge/SpaceFlightChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceFlightChallengeApp.swift
+//  SpaceFlightChallenge
+//
+//  Created by Daniel esteban Bermudez valenzuela on 22/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SpaceFlightChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
